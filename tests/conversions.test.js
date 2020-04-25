9593b18ad055e4d06b25cd237b8bb4aa4fbfff43
David Eastmond <davebuck1981@gmail.com>
2020-04-24 21:53:04 -0400

    using only for non-api tests

--- tests/conversions.test.js
+++ tests/conversions.test.js
@@ -1,23 +1,25 @@
 import { getWindCompassDirectionFromDegrees, getWindSpeed } from "../electron/scripts/weather-functions.js";
 
-test("converts wind direction in degrees to compass coordinates (90 degrees)", ()=> {
-  const result = getWindCompassDirectionFromDegrees(90);
-  expect(result).toBe("E");
-});
+describe.only("Conversion tests", ()=> {
+  test("converts wind direction in degrees to compass coordinates (90 degrees)", ()=> {
+    const result = getWindCompassDirectionFromDegrees(90);
+    expect(result).toBe("E");
+  });
 
-test("converts wind direction in degrees to compass coordinates (0 degrees)", ()=> {
-  const result = getWindCompassDirectionFromDegrees(0);
-  expect(result).toBe("N");
-});
+  test("converts wind direction in degrees to compass coordinates (0 degrees)", ()=> {
+    const result = getWindCompassDirectionFromDegrees(0);
+    expect(result).toBe("N");
+  });
 
-test("calculates correct wind speed, converting from m/s to km/h", ()=> {
-  const speedValue = 30;
-  const result = getWindSpeed(speedValue, "metric");
-  expect(result).toBe(Math.ceil(108));
-});
+  test("calculates correct wind speed, converting from m/s to km/h", ()=> {
+    const speedValue = 30;
+    const result = getWindSpeed(speedValue, "metric");
+    expect(result).toBe(Math.ceil(108));
+  });
 
-test("calculates correct wind speed, imperial", ()=> {
-  const speedValue = 30;
-  const result = getWindSpeed(speedValue, "imperial");
-  expect(result).toBe(30);
+  test("calculates correct wind speed, imperial", ()=> {
+    const speedValue = 30;
+    const result = getWindSpeed(speedValue, "imperial");
+    expect(result).toBe(30);
+  });
 });
