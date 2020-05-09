8dbb524e4f39602e04ad2f025570f995e5d8060f
David Eastmond <davebuck1981@gmail.com>
2020-05-08 21:14:14 -0400

    removed test only

--- tests/local-storage.test.js
+++ tests/local-storage.test.js
@@ -3,7 +3,7 @@ import { getFromLocalStorage } from "../electron/scripts/weather-functions";
 import { FakeStorage } from "./utils/fake-storage";
 
 describe("local storage tests", () => {
-  test.only("should return the correct unit of measurements when retrieving from local storage", ()=> {
+  test("should return the correct unit of measurements when retrieving from local storage", ()=> {
     const fs1 = new FakeStorage("units", "imperial");
     const fs2 = new FakeStorage("units", "metric");
     expect(getMeasurementUnitsFromLocalStorage(fs1)).toBe("imperial");
