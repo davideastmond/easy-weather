5409a05f7570ca9fd1c8c69f7b54209325c35b5a
David Eastmond <davebuck1981@gmail.com>
2020-05-09 12:24:28 -0400

    add some storage object stuff

--- tests/fake-storage.test.js
+++ tests/fake-storage.test.js
@@ -6,9 +6,23 @@ describe("Fake storage test functionality", ()=> {
     const result = fakeStorage.getItem("foo");
     expect(result).toBe("bar");
   });
+
   test("fake storage object returns undefined due to invalid key", ()=> {
     const fakeStorage = new FakeStorage("foo", "bar");
     const result = fakeStorage.getItem("other");
     expect(result).toBe(undefined);
   }); 
+
+  test("fake storage object sets the correct key value (where value is a string) and it can be retrieved", ()=> {
+    const fakeStorage = new FakeStorage();
+    // create a new key value (string) on the fly and then test that it can be retrieved
+    fakeStorage.setItem("foo", "bar"); 
+    expect(fakeStorage.getItem("foo")).toBe("bar");
+  });
+
+  test.only("fake storage object sets key value but value is an object", () => {
+    const fakeStorage = new FakeStorage();
+    fakeStorage.setItem("foo", { key: "value"});
+    expect(fakeStorage.getItem("foo")).toBe("value");
+  });
 });
--- tests/utils/fake-storage.js
+++ tests/utils/fake-storage.js
@@ -1,3 +1,7 @@
+/**
+ * This class simulats basic localStorage functionality, allowing users to set
+ * dynamic key values and retrieve them as long as the object is in scope
+ */
 export class FakeStorage {
   constructor(key, val) {
    this.prop = {
@@ -7,6 +11,12 @@ export class FakeStorage {
 
   getItem(key) {
     return this.prop[key];
+  }
+
+  setItem(key, val) {
+    this.prop = {
+      [key]: val
+    };
   }
 
   saveObject(key, val) {
