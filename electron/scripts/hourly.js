68d42dfae940d3864610790e1119c39e6341d339
davideastmond <davebuck1981@gmail.com>
2020-05-16 09:51:29 -0400

    created hourly strip template

--- electron/scripts/hourly.js
+++ electron/scripts/hourly.js
@@ -17,3 +17,26 @@ function liveLoadBackgroundImage(fileName) {
   const fn = `../img/backgrounds/${fileName}`;
   $(".hourly-window").css("background-image", "url(" + fn + ")");
 }
+
+/**
+ * an hourlyStrip is a row of hourly data from the API.
+ * This is a template
+ *  
+ */
+export const hourlyStrip = ({
+  styling,
+  dateTime,
+  temp,
+  feels_like,
+  icon,
+  desc,
+  wind
+}) => `
+<tr class="${styling}">
+  <td>${dateTime}</td>
+  <td>${temp}</td>
+  <td>${feels_like}</td>
+  <td>${icon}</td>
+  <td>${desc}</td>
+  <td>${wind}</td>
+</tr>`;
